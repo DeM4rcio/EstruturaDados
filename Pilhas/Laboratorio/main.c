29dd34d2f92262672e5bbafa12ef06dce8e5a2af
DeM4rcio <henriquemarcio170@gmail.com>
2023-12-15 20:09:53 -0300

    Remapeando a função de empilhamento

--- Pilhas/Laboratorio/main.c
+++ Pilhas/Laboratorio/main.c
@@ -21,29 +21,52 @@ No* desempilhar(No **topo)
 No* empilhar(No *topo, char x)
 {
     No *novo = malloc(sizeof(No));
-    if(topo)
+    if(novo)
     {
         novo->caracter = x;
         novo->proximo = topo ;
+        return novo;
     }else{
-        printf("A pilha não tem topo");
+        printf("A pilha nao tem topo");
     }return NULL;
     
 }
 
 int indendifica_formacao(char x[])
-{
+{   
+    No *topo = NULL;
+    
+    
+    int tamanho = sizeof(x) / sizeof(x[0]);
+    for(int i = 0; i <= tamanho;i++)
+    {
+        printf("%c", x[i]);
+        topo = empilhar(topo,x[i]);
+    }
+    ImprimirPilha(topo);
+    return 0;
+}
 
+void ImprimirPilha(No *topo)
+{
+    printf("\n ----------------PILHA-----------\n");
+    while (topo)
+    {
+        printf("%*c", &topo->caracter);
+        topo = topo->proximo;
+    }
+    printf("\n ----------------PILHA-----------\n");
 }
 
 
 int main()
 {
     char exp[50];
-
     printf("\nDigite uma expressao: ");
     scanf("%49[^\n]", exp);
     printf("\nExpressao: %s\nRetorno: %d\n",exp, indendifica_formacao(exp));
+
+    
 
     return 0;
 }
